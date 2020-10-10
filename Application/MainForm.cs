3b8b8dac026d064a6aa10674154d0673609a3290
Mihail Fedotov <Dizel_penk@mail.ru>
2020-10-10 08:24:08 +0300

    Добавил базовую форму MainForm

---
+++ Application/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application
+{
+    public partial class MainForm : Form
+    {
+        EmploeeBank emploee = new EmploeeBank(55555, "Ktoto", "password");
+        public MainForm()
+        {
+            InitializeComponent();
+            User.ReadFromFileAllClients();
+            AllBankAccounts.ReadFromFileAllClients();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmploeePanel panel = new EmploeePanel(emploee);
+            panel.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AutorizationPanel panel = new AutorizationPanel();
+            panel.Show();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
